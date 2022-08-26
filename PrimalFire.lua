837704f7b3b47271cbe7b0c9f0294dd280e2e78b
PGSleepy <97898191+pgsleepy@users.noreply.github.com>
2022-08-26 21:05:24 +0200

    Update PrimalFire.lua

--- PrimalFire.lua
+++ PrimalFire.lua
@@ -5,20 +5,21 @@ local function DoReadyAndRotation()
     total_times = total_times + 1;
     if (total_times > 4) then
         total_times = 0
-        SendChatMessage("[PrimalFire ADDON]: Last run before rotations! (5/5)", "PARTY", "Common");
-        SendChatMessage("[PrimalFire ADDON]: Resetting counter!", "PARTY", "Common");
+        SendChatMessage("[Arlow's PrimalFire ADDON]: Last run before rotations! (5/5)", "PARTY", "Common");
+        SendChatMessage("[Arlow's PrimalFire ADDON]: Resetting counter!", "PARTY", "Common");
+        DoReadyCheck();
         return
     end
     DoReadyCheck()
-    SendChatMessage("[PrimalFire ADDON]: This will be run " .. total_times .. "/5", "PARTY", "Common");
+    SendChatMessage("[Arlow's PrimalFire ADDON]: This will be run " .. total_times .. "/5!", "PARTY", "Common");
 end
 
 local f = CreateFrame("Frame")
 f:RegisterEvent("CHAT_MSG_SYSTEM")
 f:SetScript("OnEvent", function(self, event, msg)
-    if strfind(msg, "Everyone is Ready") then
+    if strfind(msg, "Everyone is Ready") then --Check if the System Message contains the string "ready".
         PlaySound(5274)
-        SendChatMessage("[PrimalFire ADDON]: Everyone is ready, let's start!", "PARTY", "Common");
+        SendChatMessage("[Arlow's PrimalFire ADDON]: Everyone is ready, let's start!", "PARTY", "Common");
         DEFAULT_CHAT_FRAME.editBox:SetText("/countdown 105")
         ChatEdit_SendText(DEFAULT_CHAT_FRAME.editBox, 0)
     end
